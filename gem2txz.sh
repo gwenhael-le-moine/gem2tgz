ccdd2a62693869ffb4e57524abc0a8f431a6b2d4
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2009-05-13 20:43:56 +0700

    first commit

---
+++ gem2txz.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+GEM=$1
+
+GEMSROOT=$(gem environment gemdir)
+
+TMP=/tmp
+PKG=$TMP/packagingagem
+
+rm -fr $PKG
+
+mkdir -p $PKG$GEMSROOT
+
+gem install $GEM \
+    --install-dir $PKG$GEMSROOT \
+    --bindir $PKG/usr/bin \
+    --force \
+    --ignore-dependencies
+
+cd $PKG
+
+NAME=$(grep "s\.name = " $PKG$GEMSROOT/specifications/*.gemspec | grep -o "%q{.*}" | sed 's|%q{||' | tr -d '}')
+VERSION=$(grep "s\.version = " $PKG$GEMSROOT/specifications/*.gemspec | grep -o "\".*\"" | tr -d '"')
+
+mkdir -p $PKG/install
+cat <<EOF > $PKG/install/slack-desc
+$NAME: $NAME ($(grep "s\.summary = " $PKG$GEMSROOT/specifications/*.gemspec | grep -o "%q{.*}" | sed 's|%q{||' | tr -d '}'))
+$NAME:
+$NAME: $(grep "s\.description = " $PKG$GEMSROOT/specifications/*.gemspec | grep -o "%q{.*}" | sed 's|%q{||' | tr -d '}')
+$NAME:
+$NAME:
+$NAME:
+$NAME:
+$NAME:
+$NAME:
+$NAME: $(grep "s\.homepage = " $PKG$GEMSROOT/specifications/*.gemspec | grep -o "%q{.*}" | sed 's|%q{||' | tr -d '}')
+$NAME:
+EOF
+
+makepkg --linkadd y --chown n $TMP/$NAME-$VERSION.txz
